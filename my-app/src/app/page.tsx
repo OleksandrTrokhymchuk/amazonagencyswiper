c2027cbc64ba6167d829522e4b2ad100cd12143d
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-16 19:45:28 +0300

    delete unccesesary h1

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,6 @@ import MainSwiper from "./components/mainSwiper/mainSwiper";
 export default function Home() {
   return (
     <>
-      <h1>amazon agency project</h1>
       <MainSwiper/>
     </> 
   )
