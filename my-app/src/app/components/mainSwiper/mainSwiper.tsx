ce7ceece4a08f3a7fd29814a0ac12ce2019bebca
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-16 20:49:17 +0300

    added more slides

--- my-app/src/app/components/mainSwiper/mainSwiper.tsx
+++ my-app/src/app/components/mainSwiper/mainSwiper.tsx
@@ -33,7 +33,102 @@ export default function MainSwiper() {
                                 <Image src={EthanMorganProfileImg} alt="Ethan Morgan Profile Picture"/>
                                 <div>
                                     Ethan Morgan
-                                    <span>Founter and CEO,<br />Serene Living Products</span>
+                                    <span>Founter and CEO, Serene Living Products</span>
+                                </div>
+                            </cite>
+                        </footer>
+                    </blockquote>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={StarlightCreationsLogoImg} alt="Starlight Creations Logo"/>
+                    <blockquote>
+                        <p>
+                            Sales Fortuna has made sales so much easier for us. It saves time, simplifies the whole process, and helps us land more deals without extra hassle.
+                        </p>
+                        <span>“</span>
+                        <footer>
+                            <cite>
+                                <Image src={OliviaHayesImg} alt="Olivia Hayes Profile Picture"/>
+                                <div>
+                                    Olivia Hayes
+                                    <span>Owner, Starlight Creations</span>
+                                </div>
+                            </cite>
+                        </footer>
+                    </blockquote>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={OpulentLivingGroupLogoImg} alt="Opulent Living Group Logo"/>
+                    <blockquote>
+                        <p>
+                            Sales Fortuna has simplified our lead generation, helping us attract qualified prospects effortlessly and drive consistent growth.
+                        </p>
+                        <span>“</span>
+                        <footer>
+                            <cite>
+                                <Image src={AlexandrReedProfileImg} alt="Alexandr Reed Profile Picture"/>
+                                <div>
+                                    Alexandr Reed
+                                    <span>Co-Founder, Opulent Living Group</span>
+                                </div>
+                            </cite>
+                        </footer>
+                    </blockquote>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={SereneLivingLogoImg} alt="Serene Living Logo"/>
+                    <blockquote>
+                        <p>
+                            Sales Fortuna made managing sales easier and helped us focus on customers. Its tools have been crucial for our growth and client satisfaction.
+                        </p>
+                        <span>“</span>
+                        <footer>
+                            <cite>
+                                <Image src={EthanMorganProfileImg} alt="Ethan Morgan Profile Picture"/>
+                                <div>
+                                    Ethan Morgan
+                                    <span>Founter and CEO, Serene Living Products</span>
+                                </div>
+                            </cite>
+                        </footer>
+                    </blockquote>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={StarlightCreationsLogoImg} alt="Starlight Creations Logo"/>
+                    <blockquote>
+                        <p>
+                            Sales Fortuna has made sales so much easier for us. It saves time, simplifies the whole process, and helps us land more deals without extra hassle.
+                        </p>
+                        <span>“</span>
+                        <footer>
+                            <cite>
+                                <Image src={OliviaHayesImg} alt="Olivia Hayes Profile Picture"/>
+                                <div>
+                                    Olivia Hayes
+                                    <span>Owner, Starlight Creations</span>
+                                </div>
+                            </cite>
+                        </footer>
+                    </blockquote>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={OpulentLivingGroupLogoImg} alt="Opulent Living Group Logo"/>
+                    <blockquote>
+                        <p>
+                            Sales Fortuna has simplified our lead generation, helping us attract qualified prospects effortlessly and drive consistent growth.
+                        </p>
+                        <span>“</span>
+                        <footer>
+                            <cite>
+                                <Image src={AlexandrReedProfileImg} alt="Alexandr Reed Profile Picture"/>
+                                <div>
+                                    Alexandr Reed
+                                    <span>Co-Founder, Opulent Living Group</span>
                                 </div>
                             </cite>
                         </footer>
