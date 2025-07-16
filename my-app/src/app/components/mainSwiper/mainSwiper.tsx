046c8f61ec317f97c885847bdd7cf6f9aedc0f79
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-17 01:22:23 +0300

    add more slides

--- my-app/src/app/components/mainSwiper/mainSwiper.tsx
+++ my-app/src/app/components/mainSwiper/mainSwiper.tsx
@@ -34,7 +34,6 @@ export default function MainSwiper() {
                             border border-[#B6B1B1] border-solid 
                             pb-8 px-7 pt-[170px]"
                     >
-                        
                         <blockquote>
                             <p className="relative text-[#9B9A99] leading-[1.7] text-xl mb-10 ">
                                 Sales Fortuna made managing sales easier and helped us focus on customers. Its tools have been crucial for our growth and client satisfaction.
@@ -56,7 +55,155 @@ export default function MainSwiper() {
                             </footer>
                         </blockquote>
                     </div>
+                </SwiperSlide>
 
+                <SwiperSlide>
+                    <div 
+                        className="max-w-[380px] max-h-[500px]
+                            bg-white rounded-[12px] 
+                            border border-[#B6B1B1] border-solid 
+                            pb-8 px-7 pt-[170px]"
+                    >
+                        <blockquote>
+                            <p className="relative text-[#9B9A99] leading-[1.7] text-xl mb-10 ">
+                                Sales Fortuna has made sales so much easier for us. It saves time, simplifies the whole process, and helps us land more deals without extra hassle.                                
+                                <span className="font-staatliches absolute right-0 bottom-[-35px] text-8xl text-[#C6C5C6] transform rotate-180">“</span>
+                                <Image 
+                                    src={StarlightCreationsLogoImg} 
+                                    alt="Starlight Creations Logo"
+                                    className="absolute top-[-85px]"
+                                />
+                            </p>
+                            <footer>
+                                <cite className="flex gap-x-4">
+                                    <Image src={OliviaHayesImg} alt="Olivia Hayes Profile Picture"/>
+                                    <div className="font-bold text-2xl tranform translate-y-3">
+                                        Olivia Hayes <br />
+                                        <span className="font-medium text-xl">Owner, Starlight <br /> Creations</span>
+                                    </div>
+                                </cite>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div 
+                        className="max-w-[380px] max-h-[500px]
+                            bg-white rounded-[12px] 
+                            border border-[#B6B1B1] border-solid 
+                            pb-8 px-7 pt-[170px]"
+                    >
+                        <blockquote>
+                            <p className="relative text-[#9B9A99] leading-[1.7] text-xl mb-10 ">
+                                Sales Fortuna has simplified our lead generation, helping us attract qualified prospects effortlessly and drive consistent growth.                                
+                                <span className="font-staatliches absolute right-0 bottom-[-35px] text-8xl text-[#C6C5C6] transform rotate-180">“</span>
+                                <Image 
+                                    src={OpulentLivingGroupLogoImg} 
+                                    alt="Opulent Living Group Logo"
+                                    className="absolute top-[-95px]"
+                                />
+                            </p>
+                            <footer>
+                                <cite className="flex gap-x-4">
+                                    <Image src={AlexandrReedProfileImg} alt="Alexandr Reed Profile Picture"/>
+                                    <div className="font-bold text-2xl tranform translate-y-3">
+                                        Olivia Hayes <br />
+                                        <span className="font-medium text-xl">Co-Founder, Opulent Living Group</span>
+                                    </div>
+                                </cite>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div 
+                        className="max-w-[380px] max-h-[500px]
+                            bg-white rounded-[12px] 
+                            border border-[#B6B1B1] border-solid 
+                            pb-8 px-7 pt-[170px]"
+                    >
+                        <blockquote>
+                            <p className="relative text-[#9B9A99] leading-[1.7] text-xl mb-10 ">
+                                Sales Fortuna made managing sales easier and helped us focus on customers. Its tools have been crucial for our growth and client satisfaction.
+                                <span className="font-staatliches absolute right-0 bottom-[-35px] text-8xl text-[#C6C5C6] transform rotate-180">“</span>
+                                <Image 
+                                    src={SereneLivingLogoImg} 
+                                    alt="Serene Living Logo"
+                                    className="absolute top-[-115px]"
+                                />
+                            </p>
+                            <footer>
+                                <cite className="flex gap-x-4">
+                                    <Image src={EthanMorganProfileImg} alt="Ethan Morgan Profile Picture"/>
+                                    <div className="font-bold text-2xl tranform translate-y-3">
+                                        Ethan Morgan <br />
+                                        <span className="font-medium text-xl">Founter and CEO, <br /> Serene Living Products</span>
+                                    </div>
+                                </cite>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div 
+                        className="max-w-[380px] max-h-[500px]
+                            bg-white rounded-[12px] 
+                            border border-[#B6B1B1] border-solid 
+                            pb-8 px-7 pt-[170px]"
+                    >
+                        <blockquote>
+                            <p className="relative text-[#9B9A99] leading-[1.7] text-xl mb-10 ">
+                                Sales Fortuna has made sales so much easier for us. It saves time, simplifies the whole process, and helps us land more deals without extra hassle.                                
+                                <span className="font-staatliches absolute right-0 bottom-[-35px] text-8xl text-[#C6C5C6] transform rotate-180">“</span>
+                                <Image 
+                                    src={StarlightCreationsLogoImg} 
+                                    alt="Starlight Creations Logo"
+                                    className="absolute top-[-85px]"
+                                />
+                            </p>
+                            <footer>
+                                <cite className="flex gap-x-4">
+                                    <Image src={OliviaHayesImg} alt="Olivia Hayes Profile Picture"/>
+                                    <div className="font-bold text-2xl tranform translate-y-3">
+                                        Olivia Hayes <br />
+                                        <span className="font-medium text-xl">Owner, Starlight <br /> Creations</span>
+                                    </div>
+                                </cite>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div 
+                        className="max-w-[380px] max-h-[500px]
+                            bg-white rounded-[12px] 
+                            border border-[#B6B1B1] border-solid 
+                            pb-8 px-7 pt-[170px]"
+                    >
+                        <blockquote>
+                            <p className="relative text-[#9B9A99] leading-[1.7] text-xl mb-10 ">
+                                Sales Fortuna has simplified our lead generation, helping us attract qualified prospects effortlessly and drive consistent growth.                                
+                                <span className="font-staatliches absolute right-0 bottom-[-35px] text-8xl text-[#C6C5C6] transform rotate-180">“</span>
+                                <Image 
+                                    src={OpulentLivingGroupLogoImg} 
+                                    alt="Opulent Living Group Logo"
+                                    className="absolute top-[-95px]"
+                                />
+                            </p>
+                            <footer>
+                                <cite className="flex gap-x-4">
+                                    <Image src={AlexandrReedProfileImg} alt="Alexandr Reed Profile Picture"/>
+                                    <div className="font-bold text-2xl tranform translate-y-3">
+                                        Olivia Hayes <br />
+                                        <span className="font-medium text-xl">Co-Founder, Opulent Living Group</span>
+                                    </div>
+                                </cite>
+                            </footer>
+                        </blockquote>
+                    </div>
                 </SwiperSlide>
             </Swiper>
         </div>
