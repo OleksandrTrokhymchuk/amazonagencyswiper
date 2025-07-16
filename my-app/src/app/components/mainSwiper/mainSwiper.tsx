09c049c8b6bb8799186e7aca0f391827df9ada81
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-16 20:22:00 +0300

    imlement all pictures into slides

--- my-app/src/app/components/mainSwiper/mainSwiper.tsx
+++ my-app/src/app/components/mainSwiper/mainSwiper.tsx
@@ -3,6 +3,14 @@
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation, Pagination, A11y } from 'swiper/modules'
 
+import SereneLivingLogoImg from "../../../../public/images/Serene_Living.svg"
+import StarlightCreationsLogoImg from "../../../../public/images/Starlight_Creations.svg"
+import OpulentLivingGroupLogoImg from "../../../../public/images/Opulent_Living_Group.svg"
+import EthanMorganProfileImg from "../../../../public/images/Ethan_Morgan.svg"
+import OliviaHayesImg from "../../../../public/images/Olivia_Hayes.svg"
+import AlexandrReedProfileImg from "../../../../public/images/Alexandr_Reed.svg"
+import Image from "next/image"
+
 export default function MainSwiper() {
     return(
         <> 
@@ -14,27 +22,37 @@ export default function MainSwiper() {
               scrollbar={{ draggable: true }}
             >
                 <SwiperSlide>
-                    Slide 1
+                    <Image src={SereneLivingLogoImg} alt="Serene Living Logo"/>
+                    <Image src={EthanMorganProfileImg} alt="Ethan Morgan Profile Picture"/>
+                    
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    Slide 2
+                    <Image src={StarlightCreationsLogoImg} alt="Starlight Creations Logo"/>
+                    <Image src={OliviaHayesImg} alt="Olivia Hayes Profile Picture"/>
+
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    Slide 3
+                    <Image src={OpulentLivingGroupLogoImg} alt="Opulent Living Group Logo"/>
+                    <Image src={AlexandrReedProfileImg} alt="Alexandr Reed Profile Picture"/>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    Slide 4
+                    <Image src={SereneLivingLogoImg} alt="Serene Living Logo"/>
+                    <Image src={EthanMorganProfileImg} alt="Ethan Morgan Profile Picture"/>
+                    
                 </SwiperSlide>
 
-                  <SwiperSlide>
-                    Slide 5
-                </SwiperSlide>
-                
                 <SwiperSlide>
-                    Slide 6
+                    <Image src={StarlightCreationsLogoImg} alt="Starlight Creations Logo"/>
+                    <Image src={OliviaHayesImg} alt="Olivia Hayes Profile Picture"/>
+
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={OpulentLivingGroupLogoImg} alt="Opulent Living Group Logo"/>
+                    <Image src={AlexandrReedProfileImg} alt="Alexandr Reed Profile Picture"/>
                 </SwiperSlide>
             </Swiper>
         </>
